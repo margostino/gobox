ec612923439ea7b0fc906bc5cc6a909cdf763ea3
Martin D'Agostino <martin.dagostino@klarna.com>
2023-02-12 22:01:53 +0100

    data extractor

---
+++ data-extractor/main.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"archive/zip"
+	"encoding/csv"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+func readCSVFromUrl(url string) ([][]string, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+	reader := csv.NewReader(resp.Body)
+	reader.Comma = ';'
+	data, err := reader.ReadAll()
+	if err != nil {
+		return nil, err
+	}
+
+	return data, nil
+}
+
+func main() {
+	url := "https://api.worldbank.org/v2/en/indicator/SI.POV.DDAY?downloadformat=csv"
+
+	out, err := os.Create("test.zip")
+	defer out.Close()
+	if err != nil {
+		fmt.Printf("err: %s", err)
+	}
+
+	resp, err := http.Get(url)
+	defer resp.Body.Close()
+	n, err := io.Copy(out, resp.Body)
+	print(n)
+
+	dst := "output"
+	archive, err := zip.OpenReader("test.zip")
+	if err != nil {
+		panic(err)
+	}
+	defer archive.Close()
+
+	for _, f := range archive.File {
+		filePath := filepath.Join(dst, f.Name)
+		fmt.Println("unzipping file ", filePath)
+
+		if !strings.HasPrefix(filePath, filepath.Clean(dst)+string(os.PathSeparator)) {
+			fmt.Println("invalid file path")
+			return
+		}
+		if f.FileInfo().IsDir() {
+			fmt.Println("creating directory...")
+			os.MkdirAll(filePath, os.ModePerm)
+			continue
+		}
+
+		if err := os.MkdirAll(filepath.Dir(filePath), os.ModePerm); err != nil {
+			panic(err)
+		}
+
+		dstFile, err := os.OpenFile(filePath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, f.Mode())
+		if err != nil {
+			panic(err)
+		}
+
+		fileInArchive, err := f.Open()
+		if err != nil {
+			panic(err)
+		}
+
+		if _, err := io.Copy(dstFile, fileInArchive); err != nil {
+			panic(err)
+		}
+
+		dstFile.Close()
+		fileInArchive.Close()
+	}
+
+}
